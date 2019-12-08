5df9538d09aa01cf553378356225f4231eb139f7
minty0829 <32600480+minty0829@users.noreply.github.com>
2019-12-08 14:59:49 -0800

    Add files via upload
    The main file

--- main.c
+++ main.c
@@ -1,177 +1,181 @@
-// Analog accelerometer app
+// BLE Service Template
 //
-// Reads data from the ADXL327 analog accelerometer
+// Creates a service for changing LED state over BLE
 
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdio.h>
-#include <math.h>
-
-
-#include "app_error.h"
 #include "nrf.h"
-#include "nrf_delay.h"
+#include "app_util.h"
+#include "nrf_twi_mngr.h"
 #include "nrf_gpio.h"
-#include "nrf_log.h"
-#include "nrf_log_ctrl.h"
-#include "nrf_log_default_backends.h"
-#include "nrf_pwr_mgmt.h"
-#include "nrf_serial.h"
-#include "nrfx_gpiote.h"
-#include "nrfx_saadc.h"
+#include "display.h"
+#include "nrf_delay.h"
+#include "states.h"
 
+#include "simple_ble.h"
 #include "buckler.h"
-// #include "states.h"
 
-// ADC channels
-#define X_CHANNEL 0
-#define Y_CHANNEL 1
-#define Z_CHANNEL 2
+#include "max44009.h"
+#include "mpu9250.h"
 
-char* fileName = "acceleration.txt";
+// I2C manager
+NRF_TWI_MNGR_DEF(twi_mngr_instance, 5, 0);
+// Enable SoftDevice (used to get RTC running)
+nrf_sdh_enable_request();
 
-// callback for SAADC events
-void saadc_callback (nrfx_saadc_evt_t const * p_event) {
-  // don't care about adc callbacks
-}
 
-// sample a particular analog channel in blocking mode
-nrf_saadc_value_t sample_value (uint8_t channel) {
-  nrf_saadc_value_t val;
-  ret_code_t error_code = nrfx_saadc_sample_convert(channel, &val);
-  APP_ERROR_CHECK(error_code);
-  return val;
-}
 
-float supply_volt_ratio = 2.87 / 3;
 
-float convert_adc_to_g(nrf_saadc_value_t adc) {
-    float result = 0;
-    float lsb = (0.6 / (1.0 / 6)) / (2 << 11); 
-    float volts = (float)adc * lsb;
-    result = (volts - 1.5 * supply_volt_ratio) / (0.42 * supply_volt_ratio);
-    return result;
-  }
+// Intervals for advertising and connections
+static simple_ble_config_t ble_config = {
+        // c0:98:e5:49:xx:xx
+        .platform_id       = 0x49,    // used as 4th octect in device BLE address
+        .device_id         = 0x0007, // TODO: replace with your lab bench number
+        .adv_name          = "EE149 LED", // used in advertisements if there is room
+        .adv_interval      = MSEC_TO_UNITS(1000, UNIT_0_625_MS),
+        .min_conn_interval = MSEC_TO_UNITS(500, UNIT_1_25_MS),
+        .max_conn_interval = MSEC_TO_UNITS(1000, UNIT_1_25_MS),
+};
 
-  float convert_g_to_tilt(char which, float g_x, float g_y, float g_z) {
-    float tilt = 0;
-    if (which == 'x') {
-      tilt = atan(g_x/ sqrt(pow(g_y, 2) + pow(g_z, 2)));
-    } else if (which == 'y') {
-      tilt = atan(g_y/ sqrt(pow(g_x, 2) + pow(g_z, 2)));
-    } else {
-      tilt = atan(sqrt(pow(g_x, 2) + pow(g_y, 2)) / g_x);
+// 32e61089-2b22-4db5-a914-43ce41986c70
+static simple_ble_service_t led_service = {{
+    .uuid128 = {0x70,0x6C,0x98,0x41,0xCE,0x43,0x14,0xA9,
+                0xB5,0x4D,0x22,0x2B,0x89,0x10,0xE6,0x32}
+}};
+
+static simple_ble_char_t led_state_char = {.uuid16 = 0x1090};
+static simple_ble_char_t lcd_state_char = {.uuid16 = 0x1091};
+static uint8_t led_state = 0;
+static uint8_t lcd_state[185];
+// memset(lcd_state, (uint8_t)0, 185);
+
+/*******************************************************************************
+ *   State for this application
+ ******************************************************************************/
+// Main application state
+simple_ble_app_t* simple_ble_app;
+states state = ACTIVE;
+braking_states brake_state = NOT_BRAKE;
+bool tilt_begin = false;
+
+void ble_evt_write(ble_evt_t const* p_ble_evt) {
+    if (simple_ble_is_char_event(p_ble_evt, &led_state_char)) {
+      printf("Got write to LED characteristic!\n");
+      printf("%d\n", led_state);
+      if (led_state == 1) {
+        state = LEFT;
+      } else if (led_state == 2) {
+        state = RIGHT;
+      } else {
+        printf("Turning off LED!\n");
+        nrf_gpio_pin_set(BUCKLER_LED0);
+        nrf_gpio_pin_set(BUCKLER_LED1);
+      }
+      tilt_begin = false;
     }
-    return tilt * (180 / 3.14);
-  }
 
-  void write_csv_to_sd(char *fileName, float data, int length) {
-    // Initialize SD card
-    // If no header, add it
-    simple_logger_log_header("HEADER for file %s\n", fileName);
-    printf("Wrote header to SD card\n");
-    // Write sample lines
-    simple_logger_log("%f\n", data);
-    nrf_gpio_cfg_output(BUCKLER_LED0);
-    nrf_gpio_pin_clear(BUCKLER_LED0);
-    // for (int i = 0; i < length; i++) {
-    //   simple_logger_log("%f \n", data[i]);
-    // }
-  } 
+    if (simple_ble_is_char_event(p_ble_evt, &lcd_state_char)) {
 
-int main (void) {
+      display_write(lcd_state, DISPLAY_LINE_1);
+    }
+}
+
+
+int main(void) {
   ret_code_t error_code = NRF_SUCCESS;
 
-  // initialize RTT library
-  error_code = NRF_LOG_INIT(NULL);
+
+  // initialize i2c master (two wire interface)
+  nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
+  i2c_config.scl = BUCKLER_SENSORS_SCL;
+  i2c_config.sda = BUCKLER_SENSORS_SDA;
+  i2c_config.frequency = NRF_TWIM_FREQ_100K;
+  error_code = nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
   APP_ERROR_CHECK(error_code);
-  NRF_LOG_DEFAULT_BACKENDS_INIT();
+  mpu9250_init(&twi_mngr_instance);
+  printf("MPU9250 initialized\n");
+  // Initialize
 
-  // initialize analog to digital converter
-  nrfx_saadc_config_t saadc_config = NRFX_SAADC_DEFAULT_CONFIG;
-  saadc_config.resolution = NRF_SAADC_RESOLUTION_12BIT;
-  error_code = nrfx_saadc_init(&saadc_config, saadc_callback);
+  // initialize display
+  nrf_drv_spi_t spi_instance = NRF_DRV_SPI_INSTANCE(1);
+  nrf_drv_spi_config_t spi_config = {
+    .sck_pin = BUCKLER_LCD_SCLK,
+    .mosi_pin = BUCKLER_LCD_MOSI,
+    .miso_pin = BUCKLER_LCD_MISO,
+    .ss_pin = BUCKLER_LCD_CS,
+    .irq_priority = NRFX_SPI_DEFAULT_CONFIG_IRQ_PRIORITY,
+    .orc = 0,
+    .frequency = NRF_DRV_SPI_FREQ_4M,
+    .mode = NRF_DRV_SPI_MODE_2,
+    .bit_order = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST
+  };
+
+  error_code = nrf_drv_spi_init(&spi_instance, &spi_config, NULL, NULL);
   APP_ERROR_CHECK(error_code);
+  display_init(&spi_instance);
+  display_write("Hello, Human!", DISPLAY_LINE_0);
+  printf("Display initialized!\n");
 
-  // initialize analog inputs
-  // configure with 0 as input pin for now
-  nrf_saadc_channel_config_t channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(0);
-  channel_config.gain = NRF_SAADC_GAIN1_6; // input gain of 1/6 Volts/Volt, multiply incoming signal by (1/6)
-  channel_config.reference = NRF_SAADC_REFERENCE_INTERNAL; // 0.6 Volt reference, input after gain can be 0 to 0.6 Volts
- 
-  // specify input pin and initialize that ADC channel
-  channel_config.pin_p = BUCKLER_ANALOG_ACCEL_X;
-  error_code = nrfx_saadc_channel_init(X_CHANNEL, &channel_config);
-  APP_ERROR_CHECK(error_code);
+  // Setup LED GPIO
+  nrf_gpio_cfg_output(BUCKLER_LED0);
+  nrf_gpio_cfg_output(BUCKLER_LED1);
+  nrf_gpio_cfg_output(BUCKLER_LED2);
 
-  // specify input pin and initialize that ADC channel
-  channel_config.pin_p = BUCKLER_ANALOG_ACCEL_Y;
-  error_code = nrfx_saadc_channel_init(Y_CHANNEL, &channel_config);
-  APP_ERROR_CHECK(error_code);
+  // Setup BLE
+  simple_ble_app = simple_ble_init(&ble_config);
 
-  // specify input pin and initialize that ADC channel
-  channel_config.pin_p = BUCKLER_ANALOG_ACCEL_Z;
-  error_code = nrfx_saadc_channel_init(Z_CHANNEL, &channel_config);
-  APP_ERROR_CHECK(error_code);
+  simple_ble_add_service(&led_service);
 
-  nrf_gpio_pin_set(BUCKLER_SD_ENABLE);
-  nrf_gpio_pin_set(BUCKLER_SD_CS);
-  const char permissions[] = "a"; // w = write, a = append    // Start file
-  simple_logger_init(fileName, permissions);
+  simple_ble_add_characteristic(1, 1, 0, 0,
+      sizeof(led_state), (uint8_t*)&led_state,
+      &led_service, &led_state_char);
 
-    // If no header, add it
-  
+  simple_ble_add_characteristic(1, 1, 0, 1,
+      sizeof(lcd_state), lcd_state,
+      &led_service, &lcd_state_char);
 
-  // initialization complete
-  printf("Buckler initialized!\n");
-  // states state = OFF;
+  // Start Advertising
+  simple_ble_adv_only_name();
+  float y = 0;
+  float x = 0;
+  while(1) {
+    y = mpu9250_read_accelerometer().y_axis;
+    x = mpu9250_read_accelerometer().x_axis;
+    if (tilt_begin && y < 0.2 && y > -.2) {
+      state = ACTIVE;
+    } else if (y > 0.2 || y < -0.2) {
+      tilt_begin = true;
+    }
 
-  // loop forever
-  while (1) {
-    // sample analog inputs
-    nrf_saadc_value_t x_val = sample_value(X_CHANNEL);
-    nrf_saadc_value_t y_val = sample_value(Y_CHANNEL);
-    nrf_saadc_value_t z_val = sample_value(Z_CHANNEL);
+    if (x < -0.1) {
+      brake_state = BRAKE;
+    } else {
+      brake_state = NOT_BRAKE;
+    }
 
-    float x_accel =  convert_adc_to_g(x_val);
-    // display results
-    printf("x: %d\ty: %d\tz:%d\n", x_val, y_val, z_val);
-    printf("g_x: %f\n", x_accel);
-    // write_csv_to_sd(fileName, x_accel, 1);
-    // printf("Angle of x is : %f\n", convert_g_to_tilt('x', convert_adc_to_g(x_val), convert_adc_to_g(y_val), convert_adc_to_g(z_val)));
-    nrf_delay_ms(100);
+    if (brake_state == BRAKE) {
+      printf ("Braking\n");
+      nrf_gpio_pin_clear(BUCKLER_LED2);
+    } else {
+      nrf_gpio_pin_set(BUCKLER_LED2);
+    }
 
-    // TODO: complete state machine
-    // switch(state) {
-    //   case OFF: {
-    //     print_state(state);
-    //     // transition logic
-    //     if (is_button_pressed(&sensors)) {
-    //       state = OFF;
-    //     } else if (forwards_state) {
-    //         state = FORWARDS;
-    //         break;
-    //     } else if (backwards_state) {
-    //         state = BACKWARDS;
-    //         break;
-    //     } else if (right_state) {
-    //         state = RIGHT;
-    //         break;
-    //     } else if (left_state) {
-    //         state = LEFT;
-    //         break;
-    //     } else {
-    //       state = OFF;
-    //       // perform state-specific actions here
-    //       kobukiDriveDirect(0, 0);
-    //     }
-    //     break; // each case needs to end with break!
-    //   }
-    //   case FORWARDS: {
-    //     if (is_button_pressed(&sensors)) {
-    //       state = OFF;
-    //     }
-    //   }
-    // }
-  }        
+    if (state == LEFT) {
+
+      printf("Making a left turn!\n");
+      nrf_gpio_pin_toggle(BUCKLER_LED0);
+      nrf_delay_ms(500);
+    } else if (state == RIGHT) {
+      printf("Making a right turn!\n");
+        nrf_gpio_pin_toggle(BUCKLER_LED1);
+        nrf_delay_ms(500);
+    }else {
+      nrf_gpio_pin_set(BUCKLER_LED0);
+      nrf_gpio_pin_set(BUCKLER_LED1);
+      nrf_delay_ms(500);
+    }
+    
+    printf("Reading (accel): %f\n", y);
+  }
 }
+
